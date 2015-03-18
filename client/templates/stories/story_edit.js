f375ec9a531fc84488d25b0742c70895b7b2688f
frank0000 <frank_oh@hotmail.com>
2015-03-17 20:38:54 -0700

    added error handling and validation for simple story submit and edit using tmeasday:error package

--- client/templates/stories/story_edit.js
+++ client/templates/stories/story_edit.js
@@ -1,3 +1,16 @@
+Template.storyEdit.created = function() {
+  Session.set('storyEditErrors', {});
+}
+
+Template.storyEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('storyEditErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('storyEditErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.storyEdit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -7,11 +20,27 @@ Template.storyEdit.events({
     var storyProperties = {
       title: $(e.target).find('[name=title]').val()
     }
+
+    var errors = validateStory(storyProperties);
+    if (errors.title) {
+      return Session.set('storyEditErrors', errors);
+    }
+
+    var storyWithSameTitle = Stories.findOne({title: storyProperties.title});
+    if (storyWithSameTitle) {
+      if (storyWithSameTitle._id === currentStoryId) {
+        Errors.throw('No changes made');
+        return Router.go('storyPage', {_id: currentStoryId});
+      }
+      else {
+        return Errors.throw('A story with this title already exists');
+      }
+    }
     
     Stories.update(currentStoryId, {$set: storyProperties}, function(error) {
       if (error) {
         // display the error to the user
-        alert(error.reason);
+        Errors.throw(error.reason);
       } else {
         Router.go('storyPage', {_id: currentStoryId});
       }
--- client/templates/stories/story_submit.js
+++ client/templates/stories/story_submit.js
@@ -1,3 +1,16 @@
+Template.storySubmit.created = function() {
+  Session.set('storySubmitErrors', {});
+}
+
+Template.storySubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('storySubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('storySubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.storySubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -6,11 +19,22 @@ Template.storySubmit.events({
        title: $(e.target).find('[name=title]').val()
     };
     
-     Meteor.call('storyInsert', story, function(error, result) {
+    var errors = validateStory(story);
+    if (errors.title) {
+      return Session.set('storySubmitErrors', errors);
+    }
+    
+    Meteor.call('storyInsert', story, function(error, result) {
       // display the error to the user and abort
-      if (error)
-        return alert(error.reason);
-      
+      if (error) {
+        return throwError(error.reason);
+      }
+
+      // show this result but route anyway
+      if (result.storyExists) {
+        return throwError('A story with this title already exists');
+      }
+
       Router.go('storyPage', {_id: result._id});  
     });
 
