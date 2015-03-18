ee1649bf2aebf6df11b3afbd499d5196757587ca
frank0000 <frank_oh@hotmail.com>
2015-03-18 16:14:37 -0700

    added simple pub/sub for blurbs

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,7 @@ Meteor.publish('stories', function() {
   return Stories.find();
 });
 
-Meteor.publish('storyBlurbs', function() {
-  return StoryBlurbs.find();
-})
+Meteor.publish('storyBlurbs', function(storyId) {
+  check(storyId, String);
+  return StoryBlurbs.find({storyId: storyId});
+});
