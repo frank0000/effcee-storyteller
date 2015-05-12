29faa2a7139670aab7580cc55f2c675fe500b8fa
frank0000 <frank_oh@hotmail.com>
2015-05-12 12:20:38 -0700

    on created user, code fixed to add username correctly to profile

--- server/accounts.js
+++ server/accounts.js
@@ -1,5 +1,8 @@
 Accounts.onCreateUser(function(options, user) {
   // creating a profile object pre-populated with name
-  user.profile = options.profile ? options.profile : {name: options.username};
+  user.profile = options.profile? options.profile : {};
+  if (!user.profile.name) {
+    user.profile.name = options.username;
+  }
   return user;
 });
