3ef4867c173dcbeefed24f1cd38d7c7e387dbe38
frank0000 <frank_oh@hotmail.com>
2015-05-12 15:40:39 -0700

    create activity and notification for change in current author

--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -9,10 +9,20 @@ Template.notifications.helpers({
 
 Template.notificationItem.helpers({
   notificationStoryPath: function() {
-    return Router.routes.passagePage.path({storyId: this.storyId, _id: this.passageId});
+    if (this.passageId) {
+      return Router.routes.passagePage.path({storyId: this.storyId, _id: this.passageId});
+    } else {
+      return Router.routes.storyPage.path({_id: this.storyId});
+    }
   },
   notificationActivityTypeLabel: function() {
-    return this.activityType;
+    var activityLabel = this.activityType;
+    if (this.activityType.startsWith("changed author to")) {
+      activityLabel += " for";
+    } else {
+      activityLabel += " to";
+    }
+    return activityLabel;
   },
   notificationStoryTitle: function() {
     return Stories.findOne({_id: this.storyId}).title;
