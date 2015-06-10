8be8eedf5f1dc28d059b198ff2424ec63e382ba6
frank0000 <frank_oh@hotmail.com>
2015-06-10 14:57:10 -0700

    only publish passages for stories that exist and current user has access to

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,5 @@
 Meteor.publish('stories', function() {
-  if (this.userId === null) {
+  if (this.userId == null) {
     return Stories.find({isPrivate: false});
   }
   return Stories.find({$or: [{userId: this.userId}, {'collaborators.userId': this.userId}, {isPrivate: false}]});
@@ -7,6 +7,23 @@ Meteor.publish('stories', function() {
 
 Meteor.publish('passages', function(storyId) {
   check(storyId, String);
+  
+  // get the story if have proper acccess permission
+  var containingStory;
+  if (this.userId == null) {
+    console.log("nobodoy here");
+    containingStory = Stories.findOne({_id: storyId, isPrivate: false});
+  } else {
+    containingStory = Stories.findOne(
+      {$and: [{_id: storyId},  {$or: [{userId: this.userId}, {'collaborators.userId': this.userId}, {isPrivate: false}]}]});
+  }
+  
+  if (!containingStory) {
+    // story doesn't exist or don't have permission, return empty list of passages
+    return Passages.find({storyId: storyId, _id: null});
+  }
+
+  // story exists and have permission, get all passages
   return Passages.find({storyId: storyId}, {sort: {submitted: 1}});
 });
 
