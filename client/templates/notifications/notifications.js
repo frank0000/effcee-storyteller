1f0329161b6d32f3570878124d6b71be48146b1a
frank0000 <frank_oh@hotmail.com>
2015-06-15 17:19:38 -0700

    in-app notifications for added and removed collaborators.

--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -9,6 +9,13 @@ Template.notifications.helpers({
 
 Template.notificationItem.helpers({
   notificationStoryPath: function() {
+    var selectedStory = Stories.findOne(
+      {$and: [{_id: this.storyId},  
+        {$or: [{userId: Meteor.userId()}, {'collaborators.userId': Meteor.userId()}, {isPrivate: false}]}
+    ]});
+    if (!selectedStory) {
+      return "#";
+    }
     if (this.passageId) {
       return Router.routes.passagePage.path({storyId: this.storyId, _id: this.passageId});
     } 
@@ -21,18 +28,37 @@ Template.notificationItem.helpers({
     var activityLabel = this.activityType;
     if (this.activityType.startsWith("changed author to")) {
       activityLabel += " for";
-    } else {
+    } else if (this.activityType.startsWith("added")) {
       activityLabel += " to";
+    } else if (this.activityType.startsWith("removed")) {
+      activityLabel += " from";
+    }
+    if (this.activityTargetUserId === Meteor.userId()) {
+      return activityLabel.replace(getUserName(Meteor.user()), "you");
     }
     return activityLabel;
   },
   notificationStoryTitle: function() {
-    return Stories.findOne({_id: this.storyId}).title;
+    selectedStory = Stories.findOne({_id: this.storyId});
+    if (!selectedStory) {
+      return "[private story]";
+    }
+    return selectedStory.title;
   }
 })
 
 Template.notificationItem.events({
   'click a': function() {
-    Notifications.update(this._id, {$set: {read: true}});
+    var notificationsListToUpdate = null;
+    if (this.passageId) {
+      notificationsListToUpdate = 
+        Notifications.find({storyId: this.storyId, passageId: this.passageId, userId: Meteor.userId(), read: false});
+    } else {
+      notificationsListToUpdate = 
+        Notifications.find({storyId: this.storyId, userId: Meteor.userId(), read: false});
+    }
+    notificationsListToUpdate.forEach(function(notification) {
+      Notifications.update({_id: notification._id}, {$set: {read: true}});
+    });
   }
 })
